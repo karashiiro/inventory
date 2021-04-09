17628f3f79a9ceb90eeee323b33cfe32ecf26217
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-09 10:59:08 -0700

    Extract logger initialization to separate file

---
+++ logger.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func initLogging() *os.File {
+	log.SetReportCaller(true)
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Create log directory if it does not exist
+	_, err := os.Stat("log/")
+	if os.IsNotExist(err) {
+		os.Mkdir("log/", 0644)
+	}
+
+	// Create and open log file
+	var logFile *os.File
+	logFile, err = os.OpenFile("log/inventory.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+
+	return logFile
+}
