42eae10278dd27742b08886bf7c478ebb21b00d8
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-08 21:23:54 -0700

    Install logrus

--- main.go
+++ main.go
@@ -1,6 +1,37 @@
 package main
 
-func main() {
-	//
+import (
+	"io"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func initLogging() *os.File {
+	log.SetReportCaller(true)
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Create log directory if it does not exist
+	_, err := os.Stat("log/")
+	if os.IsNotExist(err) {
+		os.Mkdir("log/", 0644)
+	}
+
+	// Create and open log file
+	var logFile *os.File
+	logFile, err = os.OpenFile("log/inventory.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+
+	return logFile
 }
 
+func main() {
+	logFile := initLogging()
+	defer logFile.Close()
+
+	log.Info("Application started.")
+}
