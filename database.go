9eedcad01d41b5ff1b7418ba7c00466e5fe42061
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-11 18:19:16 -0700

    Install cache layer

--- database.go
+++ database.go
@@ -3,15 +3,19 @@ package main
 import (
 	"os"
 
+	"github.com/go-redis/cache/v8"
+	"github.com/go-redis/redis/v8"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 type Database struct {
 	db *gorm.DB
+	c  *cache.Cache
 }
 
 func initDatabase() (*Database, error) {
+	// Connect to PGL server
 	dsn := os.Getenv("INVENTORY_PGL_CONNECTION_STRING")
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
@@ -23,7 +27,17 @@ func initDatabase() (*Database, error) {
 
 	db.AutoMigrate(&PlayerItem{})
 
+	// Connect to Redis server
+	r := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("INVENTORY_REDIS_LOCATION"),
+	})
+
+	c := cache.New(&cache.Options{
+		Redis: r,
+	})
+
 	return &Database{
 		db: db,
+		c:  c,
 	}, nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/karashiiro/inventory
 go 1.16
 
 require (
-	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/go-redis/cache/v8 v8.4.0
+	github.com/go-redis/redis/v8 v8.8.0
+	github.com/sirupsen/logrus v1.8.1
 	gorm.io/driver/postgres v1.0.8
-	gorm.io/gorm v1.21.7 // indirect
+	gorm.io/gorm v1.21.7
 )
